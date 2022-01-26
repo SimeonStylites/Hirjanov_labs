46e1c2ac33d00f7ae00183b14cbf548fb97568eb
SimeonStylites <klimachkovdmitry@gmail.com>
2022-01-26 17:29:20 +0300

    Balls move

--- lab4/catch_a_ball.py
+++ lab4/catch_a_ball.py
@@ -3,7 +3,7 @@ from pygame.draw import *
 from random import randint
 pygame.init()
 
-FPS = 1
+FPS = 30
 LX, LY = 1200, 900
 screen = pygame.display.set_mode((LX, LY))
 
@@ -20,6 +20,7 @@ def new_ball():
     """
     Draw a new ball in a random place ona screen with a radius
     from r_min to r_max with a color from COLORS except BLACK
+    Return parameters of a ball
     """
     r_min, r_max = 10, 100
     margin = r_max
@@ -28,25 +29,42 @@ def new_ball():
     r = randint(10,100)
     color = COLORS[randint(0,len(COLORS)-2)]
     circle(screen, color, (x, y), r)
-    return (x,y,r)
+    return(x,y,r,color)
+
+def move_ball(x,y,r,color,vx,vy):
+    """
+    Move a ball on a vx,vy. Return new coordinates
+    radius and color of a ball
+    """
+    circle(screen, BLACK, (x, y), r)
+    x += vx
+    y += vy
+    circle(screen, color, (x, y), r)
+    return(x, y, r, color)
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 points = 0
+ticks = 0
+(x,y,r,color) = new_ball()
+vx, vy = randint(1,10), randint(1,10)
+pygame.display.update()
 
 while not finished:
+    ticks += 1
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN:
             if (event.pos[0]-x)**2+(event.pos[1]-y)**2 <= r**2:
-                points +=1
+                points += 1
+                screen.fill(BLACK)
+                (x,y,r,color) = new_ball()
+                vx, vy = randint(1,10), randint(1,10)
                 print(points)
-    
-    (x,y,r) = new_ball()
+    (x,y,r,color) = move_ball(x,y,r,color,vx,vy)
     pygame.display.update()
-    screen.fill(BLACK)
 
 pygame.quit()
