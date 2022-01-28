6869a88953fb8295503bc3bb03cf93abe2f406dc
SimeonStylites <klimachkovdmitry@gmail.com>
2022-01-28 20:04:43 +0300

    Balls reflect from the walls. Points are awarded for the quick hits. For the slow hits and missing fines are charged

--- lab4/catch_a_ball.py
+++ lab4/catch_a_ball.py
@@ -6,6 +6,9 @@ pygame.init()
 FPS = 30
 LX, LY = 1200, 900
 screen = pygame.display.set_mode((LX, LY))
+bonus0 = 300
+missfine = 100
+timefine = 90//FPS
 
 RED = (255,0,0)
 BLUE = (0,0,255)
@@ -24,31 +27,41 @@ def new_ball():
     """
     r_min, r_max = 10, 100
     margin = r_max
+    vmin, vmax = 1,15
     x = randint(margin, LX-margin)
     y = randint(margin, LY-margin)
     r = randint(10,100)
     color = COLORS[randint(0,len(COLORS)-2)]
     circle(screen, color, (x, y), r)
-    return(x,y,r,color)
+    vx, vy = randint(vmin,vmax), randint(vmin,vmax)
+    return(x,y,r,color,vx,vy)
 
 def move_ball(x,y,r,color,vx,vy):
     """
-    Move a ball on a vx,vy. Return new coordinates
-    radius and color of a ball
+    Move a ball on a vx,vy. Turn a ball colliding into wall. Return new coordinates
+    radius, color of a ball and new velocities.
     """
     circle(screen, BLACK, (x, y), r)
     x += vx
     y += vy
+    A = (x > LX-r) or (x < r)
+    B = (y > LY-r) or (y < r)
+    if A:
+        x -= 2*vx
+        vx = -vx
+    if B:
+        y -= 2*vy
+        vy = -vy
     circle(screen, color, (x, y), r)
-    return(x, y, r, color)
+    return(x, y, r, color, vx, vy)
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 points = 0
 ticks = 0
-(x,y,r,color) = new_ball()
-vx, vy = randint(1,10), randint(1,10)
+(x,y,r,color,vx,vy) = new_ball()
+bonus = bonus0
 pygame.display.update()
 
 while not finished:
@@ -59,12 +72,19 @@ while not finished:
             finished = True
         if event.type == pygame.MOUSEBUTTONDOWN:
             if (event.pos[0]-x)**2+(event.pos[1]-y)**2 <= r**2:
-                points += 1
+                points += bonus
+                print(points, bonus)
                 screen.fill(BLACK)
-                (x,y,r,color) = new_ball()
-                vx, vy = randint(1,10), randint(1,10)
-                print(points)
-    (x,y,r,color) = move_ball(x,y,r,color,vx,vy)
+                (x,y,r,color,vx,vy) = new_ball()
+                bonus = bonus0
+            else:
+                points -= missfine
+                print(points, -missfine)
+                screen.fill(BLACK)
+                (x,y,r,color,vx,vy) = new_ball()
+                bonus = bonus0
+    (x, y, r, color, vx, vy) = move_ball(x,y,r,color,vx,vy)
     pygame.display.update()
+    bonus -= timefine
 
 pygame.quit()
